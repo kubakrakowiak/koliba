992bb81a36b0db3fa28bbae38206f60cd0f084c7
kubakrakowiak <37554894+kubakrakowiak@users.noreply.github.com>
2018-03-27 09:41:00 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,42 @@
+var menu0 = document.getElementById("b0");
+var menu1 = document.getElementById("b1");
+var menu2 = document.getElementById("b2");
+var menu3 = document.getElementById("b3");
+var menu4 = document.getElementById("b4");
+var menu5 = document.getElementById("b5");
+var menu6 = document.getElementById("b6");
+
+
+
+
+var stara='.menu0';
+
+
+function showing(nr)
+{
+$('.menu'+stara).removeClass('menuA');
+$('.menu'+nr).addClass('menuA');
+stara=nr;
+}
+
+
+$(document).ready(function() {
+    var NavY = $('#main').offset().top;
+      
+    var stickyNav = function(){
+    var ScrollY = $(window).scrollTop();
+           
+    if (ScrollY > NavY) { 
+        $('#logo').addClass('logoS');
+    } else {
+        $('#logo').removeClass('logoS'); 
+    }
+    };
+      
+    stickyNav();
+      
+    $(window).scroll(function() {
+        stickyNav();
+    });
+    });
+
