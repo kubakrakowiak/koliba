2fa6e3486c610e3a261818cb55a8052f983fd0e9
kubakrakowiak <kubamega999kk@onet.pl>
2018-03-28 20:16:07 +0200

    SmoothScrolling

--- js/skrypt.js
+++ js/skrypt.js
@@ -43,38 +43,23 @@ $(document).ready(function() {
 
 
 
-    $("#n0").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#work").offset().top
-        }, 2000);
-    });
-    $("#n1").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#contact").offset().top
-        }, 2000);
-    });
-    $("#n2").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#party").offset().top
-        }, 2000);
-    });
-    $("#n3").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#gallery").offset().top
-        }, 2000);
-    });
-    $("#n4").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#menu").offset().top
-        }, 2000);
-    });
-    $("#n5").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#news").offset().top
-        }, 2000);
-    });
-    $("#n6").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#home").offset().top
-        }, 2000);
-    });
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+    
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+    
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+    
+    // top position relative to the document
+    var pos = $id.offset().top;
+    
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
